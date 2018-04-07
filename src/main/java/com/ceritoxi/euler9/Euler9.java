e3af8b89706c7a8945b238795a2926f82ae42dd8
ceritoxi <ceritoxi@gmail.com>
2018-04-07 14:21:40 +0200

    Task 9 of the EulerProject

---
+++ src/main/java/com/ceritoxi/euler9/Euler9.java
@@ -0,0 +1,34 @@
+package com.ceritoxi.euler9;
+
+import com.ceritoxi.util.Math;
+
+public class Euler9 {
+	
+	private int result;
+	
+	public Euler9() {
+		this.result = 0;
+		this.result = task();
+	}
+	
+	private int task() {
+		for (int i = 1; i < 1000; i++) {
+            for (int j = 1; j < 1000; j++) {
+                for (int k = 1; k < 1000; k++) {
+                    if (Math.isPythagoreanTriplet(i, j, k) && i + j + k == 1000) {
+                        return i * j * k;
+                    }
+                }
+            }
+        }
+		return 0;
+	}
+	
+	public int getResult() {
+		return this.result;
+	}
+	
+	public void run() {
+		System.out.println(this.result);
+	}
+}
---
+++ src/main/java/com/ceritoxi/euler9/Runner.java
@@ -0,0 +1,9 @@
+package com.ceritoxi.euler9;
+
+public class Runner 
+{
+    public static void main(String[] args) {
+		Euler9 euler9 = new Euler9();
+		euler9.run();
+	}
+}
