b772c4052c1e248fff03aa86fb0e4a4d33108924
ceritoxi <ceritoxi@gmail.com>
2018-04-07 14:08:33 +0200

    Task 6 of the EulerProject

---
+++ src/main/java/com/ceritoxi/euler6/Euler6.java
@@ -0,0 +1,21 @@
+package com.ceritoxi.euler6;
+
+import com.ceritoxi.util.Math;
+
+public class Euler6 {
+	
+	private int result;
+	
+	public Euler6() {
+		this.result = 0;
+		this.result = Math.differenceBetweenSumOfSquaresAndSquareOfSumUnder(101);
+	}
+	
+	public int getResult() {
+		return this.result;
+	}
+	
+	public void run() {
+		System.out.println(this.result);
+	}
+}
