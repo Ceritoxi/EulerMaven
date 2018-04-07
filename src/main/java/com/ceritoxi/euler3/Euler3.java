29d7cda63766cb15bd1d8b49710e4284fdab9942
ceritoxi <ceritoxi@gmail.com>
2018-04-07 13:52:46 +0200

    Task 3 of the EulerProject

---
+++ src/main/java/com/ceritoxi/euler3/Euler3.java
@@ -0,0 +1,21 @@
+package com.ceritoxi.euler3;
+
+import com.ceritoxi.util.Math;
+
+public class Euler3 {
+	
+	private int result;
+	
+	public Euler3() {
+		this.result = 0;
+		this.result = Math.largestPrimeFactorOf(600851475143);
+	}
+	
+	public int getResult() {
+		return this.result;
+	}
+	
+	public void run() {
+		System.out.println(this.result);
+	}
+}
