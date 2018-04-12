3a28901b8dfaf6717d6a0090914371614163b48a
ceritoxi <ceritoxi@gmail.com>
2018-04-12 12:49:30 +0200

    Task 5

--- src/main/java/com/ceritoxi/euler5/Euler5.java
+++ src/main/java/com/ceritoxi/euler5/Euler5.java
@@ -7,7 +7,6 @@ public class Euler5 {
 	private int result;
 	
 	public Euler5() {
-		this.result = 0;
 		int[] argument = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 		this.result = Math.greatestCommonDivisor(argument);
 	}
