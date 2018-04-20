0e67fc8ed2e578388b88bc863f4f1be2e519c378
ceritoxi <ceritoxi@gmail.com>
2018-04-20 14:19:50 +0200

    some change

--- src/main/java/com/ceritoxi/euler1/Euler1.java
+++ src/main/java/com/ceritoxi/euler1/Euler1.java
@@ -4,18 +4,17 @@ import com.ceritoxi.util.Math;
 
 public class Euler1 {
 	
-	private int sum;
+	private int result;
 	
 	public Euler1() {
-		this.sum = 0;
-		this.sum = Math.sumOfNumbersUnderZThatAreDividableByXorY(3, 5, 1000);
+		this.result = Math.sumOfNumbersUnderZThatAreDividableByXorY(3, 5, 1000);
 	}
 	
 	public int getResult() {
-		return this.sum;
+		return this.result;
 	}
 	
 	public void run() {
-		System.out.println(this.sum);
+		System.out.println(this.result);
 	}
 }
